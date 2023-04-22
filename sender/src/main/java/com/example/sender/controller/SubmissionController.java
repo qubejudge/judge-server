db654d8f81e5c1afcb2403a8d853a97a2ca77098
pranu2502 <spranavreddy25@gmail.com>
2023-04-22 20:25:56 +0530

    azure stup

--- sender/src/main/java/com/example/sender/controller/SubmissionController.java
+++ sender/src/main/java/com/example/sender/controller/SubmissionController.java
@@ -4,6 +4,7 @@ import com.example.sender.dto.SubmissionResponse;
 import com.example.sender.services.SubmissionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,6 +19,12 @@ public class SubmissionController {
 
     @Autowired
     private final SubmissionService submissionService;
+
+    @GetMapping("/hi")
+    public ResponseEntity<String> pingEndpoint()
+    {
+        return new ResponseEntity<String>("hiiii", HttpStatusCode.valueOf(200));
+    }
 
     @PostMapping("/submit-file")
     public ResponseEntity<SubmissionResponse> submitFile(@RequestParam("file")MultipartFile file) throws IOException {
